4acc6526226da103e4ad69a7418c5bf89c9b2482
itTgame <134811656+itTgame@users.noreply.github.com>
2026-04-26 15:38:17 +0300

    Update roomba_dashboard_api.ino

--- esp32/roomba_dashboard_api/roomba_dashboard_api.ino
+++ esp32/roomba_dashboard_api/roomba_dashboard_api.ino
@@ -21,7 +21,7 @@ namespace {
 constexpr int ROOMBA_RX = 16;
 constexpr int ROOMBA_TX = 17;
 
-const char* WIFI_SSID = ".";
+const char* WIFI_SSID = "";
 const char* WIFI_PASS = "";
 const char* WIFI_HOSTNAME = "roomba-dashboard";
 const char* API_KEY = "";
